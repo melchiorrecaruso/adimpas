ca4ffe072dfc3a7ca5b0f6a8f1fd7c4a1ba0daa5
Melchiorre Caruso <melchiorrecaruso@gmail.com>
2024-04-28 22:35:24 +0200

    Updating copyright info

--- toolkit-v2/common.pas
+++ toolkit-v2/common.pas
@@ -1,7 +1,7 @@
 {
   Description: Common routines.
 
-  Copyright (C) 2023 Melchiorre Caruso <melchiorrecaruso@gmail.com>
+  Copyright (C) 2023-2024 Melchiorre Caruso <melchiorrecaruso@gmail.com>
 
   This program is free software: you can redistribute it and/or modify
   it under the terms of the GNU Lesser General Public License as published by
@@ -88,9 +88,6 @@ procedure CleanDocument(S: TStringList);
 function IsASpecialKey(const AKey: string): boolean;
 function IsAVersorKey(const AKey: string): boolean;
 function IsAVector(const AClassName: string): boolean;
-
-
-
 
 implementation
 
